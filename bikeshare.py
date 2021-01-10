9cdff762007788ff7e30f87e9ef85793635160fa
Büşra Odacı <busra.odaci@turkcell.com.tr>
2021-01-10 22:30:32 +0300

    refactor: comment line added

--- bikeshare.py
+++ bikeshare.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import click
-
+#test
 # CITY_DATA
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
