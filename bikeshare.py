75b78882019dd38cdad7b93e93d6e8dd7a136092
Büşra Odacı <busra.odaci@turkcell.com.tr>
2021-01-10 22:34:00 +0300

    refactor: weekdays is changed

--- bikeshare.py
+++ bikeshare.py
@@ -11,7 +11,7 @@ CITY_DATA = {'chicago': 'chicago.csv',
 
 months = ('january', 'february', 'march', 'april', 'may')
 
-weekdays = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
+weekdays = ('sunday', 'monday', 'tuesday', 'wednesday', 'thursday',
             'saturday')
 
 
